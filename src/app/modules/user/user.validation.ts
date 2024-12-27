046d4127d81dc3ea0ff3989aa7a45b6476eec222
mehejar <abdalmehejar@gmail.com>
2024-12-27 14:53:32 +0600

    Added All

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,11 +1,13 @@
 import { z } from "zod"
 
 const userValidationSchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string(),
-    role: z.enum(["admin", "user"]),
-    isBlocked: z.boolean()
+    body: z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string(),
+        role: z.enum(["admin", "user"]).optional(),
+        isBlocked: z.boolean().optional()
+    })
 });
 
 export const userValidation = {
